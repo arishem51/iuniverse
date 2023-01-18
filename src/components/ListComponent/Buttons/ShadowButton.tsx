51c60e34c15874a59872a6e6f7813d93e3a88a14
Arishemm <arishem51@gmail.com>
2023-01-18 16:25:14 +0700

    chore: color

--- src/components/ListComponent/Buttons/ShadowButton.tsx
+++ src/components/ListComponent/Buttons/ShadowButton.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export default styled.button`
+  --color: rgb(0, 140, 255);
   all: unset;
   padding: 10px 20px;
-  background-color: rgb(0, 140, 255);
+  background-color: var(--color);
   border-radius: 10px;
-  box-shadow: 0 0 25px rgb(0, 140, 255);
+  box-shadow: 0 0 25px var(--color);
   transition: box-shadow 0.5s;
   color: var(--color-white);
   text-transform: uppercase;
@@ -13,7 +14,7 @@ export default styled.button`
   font-weight: 700;
   cursor: pointer;
   :hover {
-    box-shadow: 0 0 5px rgb(0, 140, 255), 0 0 25px rgb(0, 140, 255),
-      0 0 50px rgb(0, 140, 255), 0 0 100px rgb(0, 140, 255);
+    box-shadow: 0 0 5px var(--color), 0 0 25px var(--color),
+      0 0 50px var(--color), 0 0 100px var(--color);
   }
 `;
