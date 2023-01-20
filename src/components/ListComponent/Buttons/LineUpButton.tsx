fc2e4b9817b2027850ea84ddf0574274a9e47dc4
Arishemm <arishem51@gmail.com>
2023-01-20 16:24:24 +0700

    chore: Stacking Context

--- src/components/ListComponent/Buttons/LineUpButton.tsx
+++ src/components/ListComponent/Buttons/LineUpButton.tsx
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 const Text = styled.h3`
   position: relative;
-  color: white;
-  z-index: 2;
-  transition: color 0.1s var(--scaleTime);
 `;
 
 const Wrapper = styled.div`
@@ -16,6 +13,12 @@ const Wrapper = styled.div`
   cursor: pointer;
   background: transparent;
   overflow: hidden;
+
+  color: white;
+  z-index: 2;
+  transition: color 0.2s var(--scaleTime);
+  font-weight: 700;
+  isolation: isolate;
   ::after {
     content: "";
     position: absolute;
@@ -26,7 +29,7 @@ const Wrapper = styled.div`
       translate var(--translateTime);
     scale: 0 1;
     transform-origin: left;
-    z-index: 1;
+    z-index: -1;
   }
   :hover {
     ::after {
@@ -35,16 +38,10 @@ const Wrapper = styled.div`
       transition: scale var(--scaleTime),
         translate var(--translateTime) var(--scaleTime);
     }
-    ${Text} {
-      color: var(--color-dark);
-    }
+    color: var(--color-dark);
   }
 `;
 
 export default function LineUpButton() {
-  return (
-    <Wrapper>
-      <Text>Hover Me</Text>
-    </Wrapper>
-  );
+  return <Wrapper>Hover Me</Wrapper>;
 }
