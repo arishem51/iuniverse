e89b6cc54cdfb858688b47b977310047bd0329cf
Arishemm <arishem51@gmail.com>
2023-01-27 13:20:48 +0700

    feat: remove unused

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import HomeLayout from "./Home";
 import HomePage from "./Home/HomePage";
