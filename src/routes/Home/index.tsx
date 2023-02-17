58d741683aadc8a2e02f05388b793cfedba49827
Arishemm <arishem51@gmail.com>
2023-02-17 10:51:54 +0700

    feat: eslint react hooks

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import AnimationLayout from "../../components/AnimationLayout";
 import { FlexContainer } from "../../components/Base";
