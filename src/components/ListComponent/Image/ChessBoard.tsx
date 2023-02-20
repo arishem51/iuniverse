3005977ce57c5ddbcc74dbb071f47ec6b67a6e53
Arishemm <arishem51@gmail.com>
2023-02-20 10:26:36 +0700

    fix: chessboard

--- src/components/ListComponent/Image/ChessBoard.tsx
+++ src/components/ListComponent/Image/ChessBoard.tsx
@@ -3,17 +3,14 @@ import styled from "styled-components";
 
 const CheckBoardWrapper = styled.div`
   --size: 200px;
-  --bg-size: calc(var(--size) / 5);
+  --bg-size: calc(var(--size) / 5) calc(var(--size) / 5) /* 40px */;
   width: var(--size);
   aspect-ratio: 1;
-  background: conic-gradient(
-      at center center,
-      var(--color-dark) 90deg,
-      var(--color-white) 0 180deg,
-      var(--color-dark) 0 270deg,
-      var(--color-white) 0 360deg
-    )
-    0 0 / var(--bg-size) var(--bg-size);
+  background: repeating-conic-gradient(
+    var(--color-dark) 0 25%,
+    var(--color-white) 0 50%
+  );
+  background-size: var(--bg-size);
 `;
 
 export default function ChessBoard() {
