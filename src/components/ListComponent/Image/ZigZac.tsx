3f32e26086ceeee891240ae63c5da0b1117c3b99
Arishemm <arishem51@gmail.com>
2023-02-20 10:51:47 +0700

    chore: css variables

--- src/components/ListComponent/Image/ZigZac.tsx
+++ src/components/ListComponent/Image/ZigZac.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const ZigZac = styled.div`
+  --color-stop: #eceddc 90deg;
   --size: 200px;
   width: var(--size);
   aspect-ratio: 1;
-  background: conic-gradient(from -45deg, #eceddc 90deg, transparent 0),
-    conic-gradient(from 135deg, #eceddc 90deg, #29ab87 0) calc(var(--size) / 4)
-      0;
+  background: conic-gradient(from -45deg, var(--color-stop), transparent 0),
+    conic-gradient(from 135deg, var(--color-stop), #29ab87 0)
+      calc(var(--size) / 4) 0;
   background-size: calc((var(--size) / 2)) calc((var(--size) / 2));
 `;
 
