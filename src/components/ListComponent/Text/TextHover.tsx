466810cc57455e65afb65df89f93a920a6adf5b4
Arishemm <arishem51@gmail.com>
2023-02-21 18:59:59 +0700

    feat: text animation

--- src/components/ListComponent/Text/TextHover.tsx
+++ src/components/ListComponent/Text/TextHover.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = styled.h1`
+  padding: 0 0.1em;
   font-size: 4em;
-  padding: 0 0.5em;
   color: var(--color-dark);
   background: linear-gradient(#1095c1 0 0) no-repeat;
   background-size: 0 100%;
   transition: background-size 0.3s, color 0.3s;
   cursor: pointer;
+  white-space: nowrap;
   :hover {
     background-position: right;
     background-size: 100% 100%;
--- src/components/ListComponent/Text/TextHoverTwo.tsx
+++ src/components/ListComponent/Text/TextHoverTwo.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = styled.h1`
-  display: block;
+  --duration: 0.3s;
+  --bg-position-delay: 0s;
+  --color-delay: 0s;
+  --bg-size-delay: var(--duration);
+  padding: 0 0.1em;
   font-size: 4em;
-  padding: 0 0.5em;
   color: var(--color-dark);
   background: linear-gradient(#1095c1 0 0) no-repeat;
   background-position: 0 1.4em;
-  background-size: 100% 100%;
+  background-size: 0 100%;
+  transition: background-position var(--duration) var(--bg-position-delay),
+    background-size var(--duration) var(--bg-size-delay),
+    color var(--duration) var(--color-delay);
   cursor: pointer;
+  white-space: nowrap;
+  :hover {
+    --bg-position-delay: var(--duration);
+    --bg-size-delay: 0s;
+    --color-delay: var(--duration);
+    background-position: 0 0;
+    background-size: 100% 100%;
+    color: var(--color-white);
+  }
 `;
 
 export default function TextHoverTwo() {
