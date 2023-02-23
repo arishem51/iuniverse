e96d6b37d86ea9d390c174a044ea24fa4a2462c8
Arishemm <arishem51@gmail.com>
2023-02-23 18:32:54 +0700

    feat: Zig Zac

--- src/components/ListComponent/Patterns/ZigZac.tsx
+++ src/components/ListComponent/Patterns/ZigZac.tsx
@@ -4,11 +4,12 @@ import { PatternContainer } from "../../Base";
 
 const ZigZac = styled(PatternContainer)`
   --color-stop: #eceddc 90deg;
+  --bg-size: calc(var(--size) / 2) calc(var(--size) / 2);
 
   background: conic-gradient(from -45deg, var(--color-stop), transparent 0),
     conic-gradient(from 135deg, var(--color-stop), #29ab87 0)
       calc(var(--size) / 4) 0;
-  background-size: calc((var(--size) / 2)) calc((var(--size) / 2));
+  background-size: var(--bg-size);
 `;
 
 export default function Component() {
