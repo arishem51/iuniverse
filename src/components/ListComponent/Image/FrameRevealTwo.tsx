20b0dbd311396e02e5cc93235c6b52c29b9f10df
Arishemm <arishem51@gmail.com>
2023-02-23 16:46:56 +0700

    feat: FrameRevealTwo

--- src/components/ListComponent/Image/FrameRevealTwo.tsx
+++ src/components/ListComponent/Image/FrameRevealTwo.tsx
@@ -2,34 +2,38 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.img`
+  --size: 200px;
   --offset: 8px;
-  --bg-animation: -200px;
+  --bg-animation: calc(var(--size) * -1);
   --bg-size: var(--offset);
+  --color-stop: var(--color-white) 270deg, transparent 0 0;
+  --animation-duration: 0.3s;
 
-  width: 200px;
+  width: var(--size);
   padding: calc(var(--offset) * 2);
   aspect-ratio: 1;
 
-  background:
-   /* conic-gradient(
+  background: conic-gradient(
         from -90deg at top var(--offset) right var(--offset),
-        var(--color-white) 270deg,
-        transparent 0 0
+        var(--color-stop)
       )
-      var(--bg-animation) 0 / 100% var(--bg-size), */ conic-gradient(
-      at bottom var(--offset) left var(--offset),
-      var(--color-white) 270deg,
-      transparent 0 0
-    )
-    bottom left / 100% 100%;
+      var(--bg-animation) 0 / 100% var(--bg-size),
+    conic-gradient(
+        from 90deg at bottom var(--offset) left var(--offset),
+        var(--color-stop)
+      )
+      0 var(--bg-animation) / var(--bg-size) 100%;
   background-repeat: no-repeat;
 
-  transition: background-position 0.3s 0.3s, background-size 0.3s;
+  transition: background-position var(--animation-duration)
+      var(--animation-duration),
+    background-size var(--animation-duration);
   cursor: pointer;
   :hover {
     --bg-animation: 0px;
     --bg-size: 100%;
-    transition: background-position 0.3s, background-size 0.3s 0.3s;
+    transition: background-position var(--animation-duration),
+      background-size var(--animation-duration) var(--animation-duration);
   }
 `;
 
