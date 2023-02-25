87f30845c49df0ee1bc4e303954efa86e5034d10
Arishemm <arishem51@gmail.com>
2023-02-25 15:15:28 +0700

    feat: corner to radial

--- src/components/ListComponent/Patterns/RadialTwo.tsx
+++ src/components/ListComponent/Patterns/RadialTwo.tsx
@@ -4,11 +4,12 @@ import { PatternContainer } from "../../Base";
 
 const Wrapper = styled(PatternContainer)`
   --bg-size: 100px 100px;
-  --color-stop: var(--color-white) 100%, transparent 0;
-  --bg-corner: closest-side;
-  background: radial-gradient(var(--bg-corner) at 25% 25%, var(--color-stop)),
-    radial-gradient(var(--bg-corner) at 75% 75%, var(--color-stop));
+  --color-stop: var(--color-white) 100%, green 0;
+  --bg-radial: closest-side;
+  background: radial-gradient(var(--bg-radial) at 25% 25%, var(--color-stop)),
+    radial-gradient(var(--bg-radial) at 75% 75%, var(--color-stop));
   background-size: var(--bg-size);
+  background-repeat: no-repeat;
 `;
 
 export default function Component() {
