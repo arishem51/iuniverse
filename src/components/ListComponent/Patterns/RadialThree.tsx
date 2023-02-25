1763fa3e71b9e6827c39a207e2f7c7d6ab1d920c
Arishemm <arishem51@gmail.com>
2023-02-25 19:06:21 +0700

    chore: space

--- src/components/ListComponent/Patterns/RadialThree.tsx
+++ src/components/ListComponent/Patterns/RadialThree.tsx
@@ -3,10 +3,15 @@ import styled from "styled-components";
 import { PatternContainer } from "../../Base";
 
 const Wrapper = styled(PatternContainer)`
-  background: radial-gradient(50px at 25% 12.5%, red 50%, transparent 0),
-    radial-gradient(50px at 25% 25%, yellow 50%, transparent 0);
-  background-size: 100px 100px;
-  background-repeat: no-repeat;
+  --color-stop: var(--color-white) 99%, transparent 0;
+  --bg-size: 50px;
+
+  background: radial-gradient(
+      calc(var(--bg-size) / 2) at top,
+      var(--color-stop)
+    ),
+    radial-gradient(calc(var(--bg-size) / 2) at bottom, var(--color-stop)) 50% 0;
+  background-size: var(--bg-size) var(--bg-size);
 `;
 
 export default function Component() {
