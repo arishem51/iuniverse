28e7acb13488a5fa9a6e66116c57375112d3c5a9
Arishemm <arishem51@gmail.com>
2023-02-25 15:11:14 +0700

    feat: closest-side

--- src/components/ListComponent/Patterns/RadialTwo.tsx
+++ src/components/ListComponent/Patterns/RadialTwo.tsx
@@ -4,8 +4,8 @@ import { PatternContainer } from "../../Base";
 
 const Wrapper = styled(PatternContainer)`
   --bg-size: 100px 100px;
-  --color-stop: var(--color-white) 50%, transparent 0;
-  --bg-corner: 50% 50%;
+  --color-stop: var(--color-white) 100%, transparent 0;
+  --bg-corner: closest-side;
   background: radial-gradient(var(--bg-corner) at 25% 25%, var(--color-stop)),
     radial-gradient(var(--bg-corner) at 75% 75%, var(--color-stop));
   background-size: var(--bg-size);
