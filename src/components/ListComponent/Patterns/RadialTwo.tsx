9feb5523215f196ba3fd8516f826eb2602a12e5c
Arishemm <arishem51@gmail.com>
2023-02-25 15:15:45 +0700

    fix: reverse work

--- src/components/ListComponent/Patterns/RadialTwo.tsx
+++ src/components/ListComponent/Patterns/RadialTwo.tsx
@@ -4,12 +4,11 @@ import { PatternContainer } from "../../Base";
 
 const Wrapper = styled(PatternContainer)`
   --bg-size: 100px 100px;
-  --color-stop: var(--color-white) 100%, green 0;
+  --color-stop: var(--color-white) 100%, transparent 0;
   --bg-radial: closest-side;
   background: radial-gradient(var(--bg-radial) at 25% 25%, var(--color-stop)),
     radial-gradient(var(--bg-radial) at 75% 75%, var(--color-stop));
   background-size: var(--bg-size);
-  background-repeat: no-repeat;
 `;
 
 export default function Component() {
