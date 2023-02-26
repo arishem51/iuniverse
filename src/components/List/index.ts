2fc6bec19b5ab04e58bf42b5a8b5f54fbb93c91e
Arishemm <arishem51@gmail.com>
2023-02-26 12:15:01 +0700

    chore: List 3d shapes

--- src/components/List/index.ts
+++ src/components/List/index.ts
@@ -1,5 +1,5 @@
 import { UIItem } from "../../types";
-import ListShapes from "./3DShapes";
+import List3DShapes from "./3DShapes";
 import ListAnimals from "./Animals";
 import ListButtons from "./Buttons";
 import ListCards from "./Cards";
@@ -7,10 +7,12 @@ import ListCheckbox from "./Checkbox";
 import ListImages from "./Images";
 import ListInputs from "./Inputs";
 import ListLoaders from "./Loaders";
+import ListShapes from "./Shapes";
 
 export const List: UIItem[] = [
   ...ListShapes,
   ...ListAnimals,
+  ...List3DShapes,
   ...ListButtons,
   ...ListCheckbox,
   ...ListInputs,
