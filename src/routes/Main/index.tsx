5e9280319d6d5d2e41f8b2729b40bee518b3637b
Arishemm <arishem51@gmail.com>
2023-02-26 11:27:32 +0700

    chore: React

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import AnimationLayout from "../../components/AnimationLayout";
 import ListComponent from "../../components/ListComponent";
