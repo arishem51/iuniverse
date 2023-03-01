202ddbbc51f286322ad287a7e8d44c2635942b5e
Arishemm <arishem51@gmail.com>
2023-03-01 17:30:26 +0700

    feat: Slider

--- src/components/List/Slider/SliderJosh.tsx
+++ src/components/List/Slider/SliderJosh.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  --color-handle: hsl(140deg 100% 70%);
+  width: 200px;
+`;
+
+const Input = styled.input`
+  -webkit-appearance: none;
+
+  width: 100%;
+  height: 0.3em;
+
+  border-radius: 0.5em;
+
+  cursor: pointer;
+  transition: opacity 0.1s;
+  :active {
+    opacity: 0.8;
+  }
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 1.75rem;
+    height: 1.75rem;
+    border-radius: 50%;
+    border: 0.25em solid var(--color-lightDark);
+    background: var(--color-handle);
+    cursor: grab;
+  }
+`;
+
 export default function Component() {
-  return <input type="range" />;
+  return (
+    <Wrapper>
+      <Input type="range" />
+    </Wrapper>
+  );
 }
