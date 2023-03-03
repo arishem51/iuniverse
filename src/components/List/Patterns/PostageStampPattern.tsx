19fe4527e99620671012f1641ed6c52b0c00f911
Arishemm <arishem51@gmail.com>
2023-03-03 08:50:19 +0700

    feat: PostageStampPattern

--- src/components/List/Patterns/PostageStampPattern.tsx
+++ src/components/List/Patterns/PostageStampPattern.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import { PatternContainer } from "../../Base";
 
-const Wrapper = styled(PatternContainer)``;
+const Wrapper = styled(PatternContainer)`
+  --bg-size: 50px;
+  --bg-position: calc(var(--bg-size) / 2 * -1);
+  --radius: 20px;
+
+  width: calc(var(--size) + var(--bg-size));
+  height: var(--size);
+
+  background: radial-gradient(
+        var(--radius),
+        var(--color-lightDark) 98%,
+        transparent 0
+      )
+      round var(--bg-position) var(--bg-position) / var(--bg-size)
+      var(--bg-size),
+    linear-gradient(to right, var(--color-white) 0 0);
+`;
 
 export default function Component() {
   return <Wrapper />;
