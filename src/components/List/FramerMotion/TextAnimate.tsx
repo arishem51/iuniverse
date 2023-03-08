7468e5670f595148ffe69e0bbd9f8a67fd817855
Arishemm <arishem51@gmail.com>
2023-03-08 15:02:01 +0700

    feat: Framer Motion Text

---
+++ src/components/List/FramerMotion/TextAnimate.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import styled from "styled-components";
+
+const Text = styled(motion.h1)`
+  color: var(--color-white);
+`;
+
+const TextItem = styled(motion.span)`
+  display: inline-block;
+  font-size: 1.5em;
+`;
+
+const variants: Variants = {
+  init: {
+    y: "40%",
+    opacity: 0,
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+  },
+  exit: {
+    y: "-40%",
+    opacity: 0,
+  },
+};
+
+export default function Component() {
+  const [state, setState] = React.useState("Hello World");
+
+  function renderItem() {
+    return state.split("").map((item, index) => {
+      if (item === " ") {
+        return <span key={"space"}>&nbsp;</span>;
+      }
+      return (
+        <TextItem
+          initial="init"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.5, delay: index * 0.25 }}
+          onAnimationComplete={(def) => {
+            if (index === state.length - 1 && def === "enter") {
+              setTimeout(() => {
+                setState((prev) => {
+                  if (prev === "Arishemm") {
+                    return "Hello World";
+                  }
+                  return "Arishemm";
+                });
+              }, 1200);
+            }
+          }}
+          layout
+          key={item + index}
+        >
+          {item}
+        </TextItem>
+      );
+    });
+  }
+
+  return (
+    <Text>
+      <AnimatePresence mode="wait">{renderItem()}</AnimatePresence>
+    </Text>
+  );
+}
--- src/components/List/FramerMotion/index.tsx
+++ src/components/List/FramerMotion/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { UIItem } from "../../../types";
 import SwitchButton from "./SwitchButton";
+import TextAnimate from "./TextAnimate";
 
 const List: UIItem[] = [
   {
     component: <SwitchButton />,
+    type: "framer",
+  },
+  {
+    component: <TextAnimate />,
     type: "framer",
   },
 ];
