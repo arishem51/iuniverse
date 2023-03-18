caeb97f31dc7fc4769ec026d71e93ae682412a8a
Arishemm <arishem51@gmail.com>
2023-03-18 18:03:57 +0700

    feat: no matter the color is :v

--- src/components/List/Images/TheRoundedFrame.tsx
+++ src/components/List/Images/TheRoundedFrame.tsx
@@ -4,15 +4,15 @@ import { ImageContainer } from "../../Base";
 const Wrapper = styled(ImageContainer)`
   --color: #fa6900;
 
-  padding: 40px;
+  padding: 30px;
 
   background: radial-gradient(20px, var(--color) 97%, #0000);
   background-repeat: repeat;
   background-size: 40px 40px;
   background-position: 0 0;
 
-  mask: conic-gradient(from 90deg at 40px 40px, transparent 25%, black 0) -20px -20px,
-    linear-gradient(#000 0 0) content-box;
+  mask: conic-gradient(from 90deg at 40px 40px, transparent 25%, #000 0) -20px -20px,
+    linear-gradient(red 0 0) content-box;
 `;
 
 export default function Component() {
