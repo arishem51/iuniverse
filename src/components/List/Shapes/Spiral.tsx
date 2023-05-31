64a37ad3329dd52d35fc3afbfd2d357fdf76f728
Arishemm <arishem51@gmail.com>
2023-05-31 17:11:31 +0700

    feat: Spiral

--- src/components/List/Shapes/Spiral.tsx
+++ src/components/List/Shapes/Spiral.tsx
@@ -1,35 +1,86 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  position: relative;
+  --child-width: 70px;
+  --child-height: 35px;
   --bd-size: 15px;
+  --position: 60px;
+
+  position: relative;
+  top: var(--position);
+  left: var(--position);
 `;
 
-const FirstChild = styled.div`
-  width: 70px;
-  height: 35px;
+const Child = styled.div`
+  position: relative;
+
   border-radius: 100px 100px 0 0;
   border-top: var(--bd-size) solid var(--color-white);
   border-right: var(--bd-size) solid var(--color-white);
   border-left: var(--bd-size) solid var(--color-white);
 `;
 
+const FirstChild = styled(Child)`
+  width: var(--child-width);
+  height: var(--child-height);
+`;
+
+const SecondChild = styled(Child)`
+  right: calc(var(--bd-size) * 2);
+  top: calc(var(--bd-size) * -1);
+
+  width: calc(var(--child-width) + var(--bd-size) * 2);
+  height: calc(var(--child-height) + var(--bd-size));
+  transform: rotate(180deg);
+`;
+
+const ThirdChild = styled(Child)`
+  top: -130px;
+  right: calc(var(--bd-size) * 2);
+
+  width: calc(var(--child-width) + var(--bd-size) * 4);
+  height: calc(var(--child-height) + var(--bd-size) * 2);
+`;
+
+const LastChild = styled(Child)`
+  top: -130px;
+  right: calc(var(--bd-size) * 4);
+
+  width: calc(var(--child-width) + var(--bd-size) * 6);
+  height: calc(var(--child-height) + var(--bd-size) * 3);
+
+  transform: rotate(180deg);
+`;
+
 const Dot = styled.div`
   --position: calc(var(--bd-size) / 2 * -1);
 
   position: relative;
-  top: var(--position);
   width: var(--bd-size);
   height: var(--bd-size);
   background: white;
   border-radius: 50%;
+  right: 0.08px;
+`;
+
+const FirstDot = styled(Dot)`
+  top: var(--position);
+`;
+
+const SecondDot = styled(Dot)`
+  top: -215px;
+  right: 60.2px;
 `;
 
 const Spiral = () => {
   return (
     <Container>
       <FirstChild />
-      <Dot />
+      <FirstDot />
+      <SecondChild />
+      <ThirdChild />
+      <LastChild />
+      <SecondDot />
     </Container>
   );
 };
