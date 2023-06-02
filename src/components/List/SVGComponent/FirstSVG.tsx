c5bd3168c97a6fc5d8c40168f11e8d1129b024fb
Arishemm <arishem51@gmail.com>
2023-06-02 21:58:03 +0700

    feat: My First SVG

--- src/components/List/SVGComponent/FirstSVG.tsx
+++ src/components/List/SVGComponent/FirstSVG.tsx
@@ -1,5 +1,38 @@
+const SIZE = 200;
+
+type AnimateProps = {
+  name: string;
+  values: string;
+};
+
+const Animate = ({ name, values }: AnimateProps) => {
+  return (
+    <animate
+      attributeName={name}
+      values={values}
+      dur="3s"
+      repeatCount="indefinite"
+    />
+  );
+};
+
 const FirstSVG = () => {
-  return <svg></svg>;
+  return (
+    <svg width={SIZE} height={SIZE} viewBox={`0 0 ${SIZE} ${SIZE}`}>
+      <g>
+        <line x1="30" x2="80" y1="2" y2="2" stroke="white" strokeWidth="2">
+          <Animate name="x1" values="30;0;30" />
+        </line>
+        <line x1="30" x2="80" y1="30" y2="80" stroke="white" strokeWidth="2">
+          <Animate name="x1" values="30;1;30" />
+          <Animate name="y1" values="30;2;30" />
+        </line>
+        <line x1="1" x2="1" y1="30" y2="80" stroke="white" strokeWidth="2">
+          <Animate name="y1" values="30;2;30" />
+        </line>
+      </g>
+    </svg>
+  );
 };
 
 export default FirstSVG;
